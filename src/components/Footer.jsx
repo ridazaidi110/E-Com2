d23b3aa6d152d1ad745e02dee26d7eb4151d0ca6
ridazaidi110 <zaidirida287@gmail.com>
2025-11-17 12:46:10 -0800

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,11 +40,11 @@ const Footer = () => {
           <div>
             <h4 className="text-lg font-semibold text-white mb-4">Contact</h4>
             <p className="text-gray-400">Email: support@shophub.com</p>
-            <p className="text-gray-400">Phone: +1 (555) 123-4567</p>
+            <p className="text-gray-400">Phone: 8941972261</p>
           </div>
         </div>
         <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 ShopHub. All rights reserved.</p>
+          <p>&copy; 2025 ShopHub. All rights reserved .</p>
         </div>
       </div>
     </motion.footer>
